d4f07ab3a98062045d84afada17e74edbd7f707e
NewMan <Danny230147@seznam.cz>
2024-02-19 16:52:43 +0100

    PripravaEditora

---
+++ editor.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+<?php
+session_start();
+
+if (!isset($_SESSION["logged_in"]) || $_SESSION["logged_in"] !== true) {
+    header("Location: index.html");
+    exit();
+}
+
+if (isset($_POST["logout"])) {
+    session_unset();
+    session_destroy();
+
+    header("Location: index.php");
+    exit();
+}
+?>
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Administrace</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="navbar.css">
+    <link rel="stylesheet" href="spravce.css">
+    <script src="javascript.js"></script>
+    <script src="spravce.js"></script>
+</head>
+
+<body>
+    <div class="pozice">
+        <div class="topnav" id="myTopnav">
+            <a href="index.php">Úvodní stránka</a>
+            <?php
+            if (!isset($_SESSION['logged_in'])) {
+                echo '<a href="" onclick="Login()">Přihlášení</a>';
+                echo '<a href="" onclick="Register()">Registrace</a>';
+            }
+            ?>
+            <a href="Rezervace.php">Rezervace</a>
+            <a href="Editor.php">Editor</a>
+            <a href="Spravce.php">Správce</a>
+
+            <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+                <i class="fa fa-bars"></i> </a>
+        </div>
+    </div>
+
+    <div class="header">
+        <form method="post">
+            <button type="submit" name="logout">Odhlásit se</button>
+        </form>
+    </div>
+    <h1> editor </h1>
+</body>
+
+</html>
