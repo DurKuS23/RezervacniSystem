76522f24a65217c935e82920c420eaa56f677e3d
Daniel Nováček <98735848+DurKuS23@users.noreply.github.com>
2024-03-23 20:39:55 +0100

    Update update_service.php

--- scripts/update_service.php
+++ scripts/update_service.php
@@ -7,8 +7,6 @@ $casSluzby = $_POST['casSluzby'];
 $casSluzby = str_replace([' ', 'min'], '', $casSluzby);
 $newCena = $_POST['cena'];
 
-echo $_POST['casSluzby'];
-
 $sql_update = "UPDATE sluzba SET typ_sluzby=?, casSluzby=?, cena=? WHERE id=?";
 $stmt = $conn->prepare($sql_update);
 $stmt->bind_param("sssi", $newTypSluzby, $casSluzby, $newCena, $updateID);
